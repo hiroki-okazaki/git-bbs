f08519cc31b8bc3175deb2e2a31e00a812255c25
hiroki-okazaki <1031okazaki@gmail.com>
2019-08-28 14:57:57 +0900

    記事表示画面コントローラー作成

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,5 +1,40 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.domain.Comment;
+import com.example.repository.ArticlesRepository;
+import com.example.repository.CommentRepository;
+
+@Controller
+@RequestMapping("/")
 public class ArticleController {
 
+	@Autowired
+	private ArticlesRepository articleRepository;
+	
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	@RequestMapping("articleList")
+	public String articleList(Model model) {
+		
+		List<Article> articleList = articleRepository.findAll();
+		
+		
+		for(Article article :  articleList) {
+			List<Comment> comment = commentRepository.findByArticleId(article.getId());
+			article.setCommentList(comment);
+		}
+		model.addAttribute("articleList",articleList);
+		return "/bbs";
+	}
+	
+	
 }
